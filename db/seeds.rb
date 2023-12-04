31cd6a88c816a7a24c5b7dddcfb7cc768a9e9989
EnnisHong <kugi7821@gmail.com>
2023-12-04 11:48:27 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "建立資料中..."
+5.times do
+  email = Faker::Internet.email
+  user = User.find_or_create_by(email:) do |u|
+    u.password = '123456'
+  end
+
+  puts "使用者:#{user.email}"
+
+  10.times do
+    product = user.products.create(title: Faker::Book.title, description: Faker::Lorem.paragraph, price: rand(10..100))
+    puts "產品:#{product.title}"
+  end
+  # user = User.find_by(email: )
+  # if not user
+  #   User.create(email: , password: '123456')
+  # end
+end
+
+puts "建立資料完成..."
